1adcb64c94d636664741b63a3667063c1cd251a3
dgumus <dgumus@outlook.com>
2018-12-05 15:10:37 +0300

    sefdfshjkbn

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,124 +1,59 @@
 package com.mycompany.app;
 import java.io.File;
-import static spark.Spark.get;
-import static spark.Spark.port;
-import static spark.Spark.post;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.jar.Attributes;
-import java.util.HashMap;
-import java.util.Map;
+import static spark.Spark.*;
 import spark.ModelAndView;
+import javax.xml.parsers.DocumentBuilder;
 import spark.template.mustache.MustacheTemplateEngine;
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import org.xml.sax.helpers.DefaultHandler;
-import org.xml.sax.SAXException;
-import java.io.IOException;
+import org.xml.sax.*;
+import java.io.*;
+import java.util.*;
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+
 
 
 public class App {
-    public static String search(String firstName, String lastName) throws SAXException, IOException, ParserConfigurationException {
-    	SAXParserFactory factory = SAXParserFactory.newInstance();
-    	SAXParser parser = factory.newSAXParser();
-    	App myobj = new App();
-    	MyHandler handler = myobj.new MyHandler();
-        parser.parse(new File("EEAS.xml"), handler);
-       
-        ArrayList<String> res=new ArrayList<>();
-        res.toString();
-        HashMap<String,ArrayList<String>> hashma = handler.getMyMap();
-        if( (!firstName.equals("")) && (!lastName.equals(""))){
-             res=hashma.get(firstName+ " " + lastName);
-             System.out.print(res.toString());
-             return res.toString();
-        }else if((!firstName.equals("")) && lastName.equals("")){
-            res= hashma.get(firstName);
-            System.out.print(res.toString());
-            return res.toString();
-        }else if( firstName.equals("") && ( !lastName.equals("") )    ){
-            res= hashma.get(lastName);
-            System.out.print(res.toString());
-            return res.toString();
-        }
-        return "";
-    	
-    }
-
-
-    class MyHandler extends DefaultHandler{
-        //key for firstname+lastname
-        HashMap<String,ArrayList<String>> hashma1 = new HashMap<>();
-    	int nameid,entityid=0;//if name is equal then print entity id not name id
-    	boolean readFirstName,readMiddleName,readLastName,readName,readWholeName = false;
-        String firstName,lastName,middleName,wholeName = "";
-        
-    	public HashMap<String,ArrayList<String>> getMyMap(){
-            return hashma1;
-        }
-    	
-    	public void startElement(String uri, String localName, String qName, Attributes attributes)throws SAXException{
-            //qName.equals("
-    		if(qName.equals("NAME")) {
-    			readName = true;
-                entityid= Integer.parseInt(attributes.getValue("Entity_id"));
-                nameid= Integer.parseInt(attributes.getValue("Entity_id"));
-            }else if(qName.equals("LASTNAME")){
-                readLastName = true;
-            }else if(qName.equals("FIRSTNAME")){
-                readFirstName = true;
-            }else if(qName.equals("MIDDLENAME")){
-                readMiddleName = true;
-            }else if(qName.equals("WHOLENAME")){
-                readWholeName = true;
+    public static String search(String input1, String input2) {
+        StringBuilder deniz  = new StringBuilder();
+        try {
+            File myfile = new File("EEAS.xml");
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+            Document doc = dBuilder.parse(myfile);
+            doc.getDocumentElement().normalize();
+            NodeList nList = doc.getElementsByTagName("ENTITY");
+            for (int temp = 0; temp < nList.getLength(); temp++) {
+                Node gez = nList.item(temp);
+                if (gez.getNodeType() == Node.ELEMENT_NODE) {
+                    Element myElm = (Element) gez;
+                    if(  !(input1.equals("") ) && input2.equals("")  ) {
+                        if(myElm.getElementsByTagName("FIRSTNAME").item(0).getTextContent().contains(input1)) {
+                            deniz.append(myElm.getAttribute("Entity_id") );
+                            deniz.append(myElm.getElementsByTagName("FIRSTNAME").item(0).getTextContent());
+                            deniz.append( myElm.getElementsByTagName("LASTNAME").item(0).getTextContent());
+                        }
+                    }else if(  input1.equals("") && !(input2.equals("") )  ) {
+                        if(myElm.getElementsByTagName("LASTNAME").item(0).getTextContent()..contains(input2)) {
+                            deniz.append( myElm.getAttribute("Entity_id"));
+                            deniz.append( myElm.getElementsByTagName("FIRSTNAME").item(0).getTextContent());
+                            deniz.append( myElm.getElementsByTagName("LASTNAME").item(0).getTextContent());
+                        }
+                    }else if(  !(input1.equals("") ) && !(input2.equals("") )  ) {
+                        if(myElm.getElementsByTagName("FIRSTNAME").item(0).getTextContent().contains(input1) && myElm.getElementsByTagName("LASTNAME").item(0).getTextContent().contains(input2)) {
+                            deniz.append( myElm.getAttribute("Entity_id"));
+                            deniz.append( myElm.getElementsByTagName("FIRSTNAME").item(0).getTextContent());
+                            deniz.append( myElm.getElementsByTagName("LASTNAME").item(0).getTextContent());
+                        }
+                    }else if(input1.equals("") && !(input2.equals(""))) {
+                        return "";
+                    }
+                }
             }
+        }catch (Exception e) {
+            e.printStackTrace();
         }
-        
-    	public void characters(char[] ch, int start, int length) throws SAXException{
-            String[] splitt ;
-    		String data = new String(ch, start, length);
-    		if(readName) {
-    			if(readFirstName) {
-    				firstName = data;
-    				readFirstName = false;
-    			}
-    			if(readLastName) {
-    				lastName = data;
-    				readLastName = false;
-    			}
-    			if(readMiddleName) {
-                    middleName = data;
-    				readMiddleName = false;
-    			}
-    			if(readWholeName) {
-                    wholeName = data;
-    				splitt = wholeName.split(" ");
-    				firstName = splitt[0];
-    				for(int i = 1; i < splitt.length - 1; i++)
-    					middleName = middleName + ' ' + splitt[i] ;
-    				lastName = splitt[splitt.length - 1];
-    				readWholeName = false;
-    			}
-    			readName = false;
-    		}
-        }
-        
-
-        public void endElement(String uri, String localName,String qName) throws SAXException{
-    		if(qName.equalsIgnoreCase("name")) {
-                ArrayList<String> entity = new ArrayList<>();
-                entity.add("" + entityid);
-                entity.add(firstName);
-                entity.add(middleName);
-                entity.add(lastName);
-                //entity.add(wholeName);
-                
-                hashma1.put(firstName + " " + lastName, entity );
-    		}
-    	}
+        return deniz.toString();
     }
-    
 
     public static void main(String[] args) {
         port(getHerokuAssignedPort());
