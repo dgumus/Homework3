6580a403563c85e30f8b24a53401b16c451d01c3
dgumus <dgumus@outlook.com>
2018-12-05 19:45:03 +0300

    s123346466

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -28,19 +28,19 @@ public class App {
                     Element myElm = (Element) gez;
                     if(  !(input1.equals("") ) && input2.equals("")  ) {
                         if(myElm.getElementsByTagName("FIRSTNAME").item(0).getTextContent().contains(input1)) {
-                            deniz.append(" " + myElm.getAttribute("Entity_id") );
+                            deniz.append(" " + myElm.getAttribute("Id") );
                             deniz.append(" " +myElm.getElementsByTagName("FIRSTNAME").item(0).getTextContent());
                             deniz.append(" " + myElm.getElementsByTagName("LASTNAME").item(0).getTextContent());
                         }
                     }else if(  input1.equals("") && !(input2.equals("") )  ) {
                         if(myElm.getElementsByTagName("LASTNAME").item(0).getTextContent().contains(input2)) {
-                            deniz.append( " " +myElm.getAttribute("Entity_id"));
+                            deniz.append( " " +myElm.getAttribute("Id"));
                             deniz.append( " " + myElm.getElementsByTagName("FIRSTNAME").item(0).getTextContent());
                             deniz.append(" " +  myElm.getElementsByTagName("LASTNAME").item(0).getTextContent());
                         }
                     }else if(  !(input1.equals("") ) && !(input2.equals("") )  ) {
                         if(myElm.getElementsByTagName("FIRSTNAME").item(0).getTextContent().contains(input1) && myElm.getElementsByTagName("LASTNAME").item(0).getTextContent().contains(input2)) {
-                            deniz.append(" " + myElm.getAttribute("Entity_id"));
+                            deniz.append(" " + myElm.getAttribute("Id"));
                             deniz.append(" " + myElm.getElementsByTagName("FIRSTNAME").item(0).getTextContent());
                             deniz.append(" " + myElm.getElementsByTagName("LASTNAME").item(0).getTextContent());
                         }
